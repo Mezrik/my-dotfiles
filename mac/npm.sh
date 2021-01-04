8c71b215777df08d169bf9c53519cd2bf8e3d399
Martin Petr <martin.petr.276@gmail.com>
2021-01-04 19:13:59 +0100

    Npm installs

---
+++ mac/npm.sh
@@ -0,0 +1,15 @@
+## Original taken from https://github.com/zellwk/dotfiles/blob/master/shared/npm.sh
+
+# Set permissions
+echo "Setting npm permissions"
+sudo chown -R $USER:$(id -gn $USER) $HOME/.config
+
+# Install commonly used clis
+echo "Downloading npm clis"
+sudo npm i -g fast-cli
+sudo npm i -g fkill-cli
+sudo npm i -g http-server
+sudo npm i -g npm-check
+sudo npm i -g standard
+sudo npm i -g svgo
+sudo npm i -g trash-cli
